e66ea0ae8c27a3f3d0a219773b75e08d1b910e57
Pawel Swiatkowski <pawel.swiatkowski@fyber.com>
2015-10-30 10:23:25 +0100

    Clean up paths

--- lib/lotus-shrine/shrine_config.rb
+++ lib/lotus-shrine/shrine_config.rb
@@ -1,4 +1,4 @@
 require 'shrine'
 require 'shrine/storage/file_system'
 
-Shrine.storages[:file_system] = Shrine::Storage::FileSystem.new("apps/web/public/uploads")
+Shrine.storages[:file_system] = Shrine::Storage::FileSystem.new("apps/web/public", subdirectory: "uploads")
